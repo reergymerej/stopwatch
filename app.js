d7ca7f82f22f66795ca5d971afae2da746b77447
Jeremy Greer <jeremy.greer@prometheanworld.com>
2015-12-08 12:34:44 -0500

    add read and reset

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 'use strict';
 
-var start = function () {};
-var stop = function () {};
-var read = function () {};
-var reset = function () {};
+var _startTime;
 
-module.exports = {
+var stopwatch;
+
+var start = function () {
+	_startTime = Date.now();
+};
+
+var stop = function () {};
+
+var read = function () {
+	if (_startTime) {
+		return Date.now() - _startTime;
+	}
+};
+
+var reset = function () {
+	return stopwatch; 
+};
+
+stopwatch = {
 	start: start,
 	stop: stop,
 	read: read,
 	reset: reset,
 };
+
+module.exports = stopwatch;
+
--- test/app.js
+++ test/app.js
@@ -3,12 +3,6 @@
 var will = require('willy').will;
 var app = require('../app.js');
 
-describe('sanity', function () {
-  it('should load', function () {
-    will(app).exist();
-  });
-});
-
 describe('api', function () {
 	it('should expose start', function () {
 		will(app.start).beA(Function);
@@ -26,3 +20,48 @@ describe('api', function () {
 		will(app.reset).beA(Function);
 	});
 });
+
+describe('reset', function () {
+	it('should be chainable', function () {
+		will(app.reset()).be(app);
+	});
+});
+
+describe('read', function () {
+	before(function () {
+		app.reset();
+	});
+
+	it('should return undefined when no timer has been started', function () {
+		will(app.read()).be(undefined);
+	});
+
+	it('should return ms since the timer started', function (done) {
+		var TIMEOUT = 10;
+		var timeout2 = TIMEOUT + 10;
+		app.start();
+
+		setTimeout(function () {
+			var result = app.read();
+			will(result >= TIMEOUT).be(true);
+		}, TIMEOUT);
+
+		setTimeout(function () {
+			var result = app.read();
+			will(result >= timeout2).be(true);
+			done();
+		}, timeout2);
+
+	});
+});
+
+describe('stop', function () {
+	before(function () {
+		app.start();
+	});
+
+	it('should stop the timer', function () {
+
+	});
+});
+
